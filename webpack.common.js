033b817b5b6105cfe211cd0c0cfc6201284d70f3
DESKTOP-MVLSK2J\Tim <gwanhyung787@gmail.com>
2018-03-12 16:43:45 +0900

    Split code

--- webpack.common.js
+++ webpack.common.js
@@ -1,38 +1,47 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 module.exports = {
     context: __dirname + '/src/screens',
     entry: {
         mainpage: ['./mainpage/index.js'],
-        reserve: ['./reserve/index.js']
+        reserve: ['./reserve/index.js'],
+        vendor: [
+            'pikaday'
+        ]
     },
-    plugins: [new CleanWebpackPlugin(['dist'])],
+    plugins: [new CleanWebpackPlugin(['dist']),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common'
+        })
+    ],
     output: {
         path: __dirname + '/dist',
-        filename: '[name].js'
+        filename: '[name].bundle.js'
     },
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /(node_modules)/,
-                options: {
-                    presets: ['@babel/preset-env']
-                }
-            }, {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }, {
-                test: /\.scss/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            }, {
-                test: /\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: 'url-loader',
-                options: {
-                    name: '[hash].[ext]',
-                    limit: 10000
-                }
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /(node_modules)/,
+            options: {
+                presets: ['@babel/preset-env']
             }
-        ]
+        }, {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }, {
+            test: /\.scss/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+        }, {
+            test: /\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: 'url-loader',
+            options: {
+                name: '[hash].[ext]',
+                limit: 10000
+            }
+        }]
     }
 };
