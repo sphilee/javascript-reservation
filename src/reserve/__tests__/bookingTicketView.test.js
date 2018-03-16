cafe4e536d187afc9b153e2ff3064b2f8dbaefa8
DESKTOP-MVLSK2J\Tim <gwanhyung787@gmail.com>
2018-03-16 16:58:04 +0900

    Add plus_minus button test

--- src/reserve/__tests__/bookingTicketView.test.js
+++ src/reserve/__tests__/bookingTicketView.test.js
@@ -13,20 +13,80 @@ describe("bookingTicketView", () => {
 
     test('initial Test', async() => {
         await page.goto(url);
+        await page.waitForSelector('.section_booking_ticket');
 
         const TOTAL_COUNT = 16;
         const resultsSelector = '.count_control_input';
-        await page.waitForSelector('.count_control_input');
 
         let totalCount = await page.evaluate(resultsSelector => {
             const tickets = Array.from(document.querySelectorAll(resultsSelector));
-            return tickets.map(e=> Number(e.value)).reduce((acc, cur) => acc + cur, 0)
+            return tickets.map(e=> +e.value).reduce((acc, cur) => acc + cur, 0)
         }, resultsSelector);
         expect(totalCount).toBe(TOTAL_COUNT);
 
         await page.waitForSelector('#totalCount');
-        totalCount = await page.$eval('#totalCount', e => Number(e.innerHTML));
+        totalCount = await page.$eval('#totalCount', e => +e.innerHTML);
         expect(totalCount).toBe(TOTAL_COUNT);
+    }, timeout);
+
+    test('plus button click Test', async() => {
+        await page.goto(url);
+        await page.waitForSelector('.section_booking_ticket');
+
+        const SELECT1_COUNT = 0;
+        const SELECT3_COUNT = 3;
+        
+        let select = 3;
+
+        let count = await page.$eval(`.qty:nth-child(${select}) .count_control_input`, e => +e.value);
+        expect(count).toBe(SELECT3_COUNT);
+
+        await page.click(`.qty:nth-child(${select}) .ico_plus3`);
+
+        count = await page.$eval(`.qty:nth-child(${select}) .count_control_input`, e => +e.value);
+        expect(count).toBe(SELECT3_COUNT+1);
+
+        select = 1;
+
+        count = await page.$eval(`.qty:nth-child(${select}) .count_control_input`, e => +e.value);
+        expect(count).toBe(SELECT1_COUNT);
+
+        await page.click(`.qty:nth-child(${select}) .ico_plus3`);
+
+        count = await page.$eval(`.qty:nth-child(${select}) .count_control_input`, e => +e.value);
+        expect(count).toBe(SELECT1_COUNT+1);
+
+
+    }, timeout);
+
+    test('minus button click Test', async() => {
+        await page.goto(url);
+        await page.waitForSelector('.section_booking_ticket');
+
+        const SELECT2_COUNT = 10;
+        const SELECT3_COUNT = 3;
+
+        let select = 3;
+
+        let count = await page.$eval(`.qty:nth-child(${select}) .count_control_input`, e => +e.value);
+        expect(count).toBe(SELECT3_COUNT);
+
+        await page.click(`.qty:nth-child(${select}) .ico_minus3`);
+
+        count = await page.$eval(`.qty:nth-child(${select}) .count_control_input`, e => +e.value);
+        expect(count).toBe(SELECT3_COUNT-1);
+
+        select = 2;
+
+        count = await page.$eval(`.qty:nth-child(${select}) .count_control_input`, e => +e.value);
+        expect(count).toBe(SELECT2_COUNT);
+
+        await page.click(`.qty:nth-child(${select}) .ico_minus3`);
+
+        count = await page.$eval(`.qty:nth-child(${select}) .count_control_input`, e => +e.value);
+        expect(count).toBe(SELECT2_COUNT-1);
+
+
     }, timeout);
 
     test('gototop click Test', async() => {
