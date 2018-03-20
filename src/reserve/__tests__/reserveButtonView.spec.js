e6e40efff519e73b76c7bc0dfc0b91e69639d0d1
DESKTOP-MVLSK2J\Tim <gwanhyung787@gmail.com>
2018-03-20 13:20:19 +0900

    Add reserveButtonView unit test

---
+++ src/reserve/__tests__/reserveButtonView.spec.js
@@ -0,0 +1,40 @@
+'use strict';
+
+import ticketModel from '../models/ticketModel';
+import ReserveButtonView from '../view/reserveButtonView';
+
+describe("reserveButtonView unit Test", () => {
+    let reserveButtonView;
+
+    beforeAll(() => {
+        // Set up our document body
+        document.body.innerHTML = `<div class="bk_btn_wrap disable">
+        <button type="button" class="bk_btn">
+            <i class="spr_book ico_naver_s"></i>
+            <span>예약하기</span>
+        </button>
+    </div>`;
+
+        reserveButtonView = new ReserveButtonView('.bk_btn_wrap');
+    });
+
+    
+    test('initial', () => {
+        expect(reserveButtonView.el.classList.contains('disable')).toBe(true);
+    });
+
+    test('ableButton', () => {
+        expect(reserveButtonView.el.classList.contains('disable')).toBe(true);
+        reserveButtonView.ableButton();
+        expect(reserveButtonView.el.classList.contains('disable')).toBe(false);
+    });
+
+    
+    test('diableButton', () => {
+        expect(reserveButtonView.el.classList.contains('disable')).toBe(false);
+        reserveButtonView.diableButton();
+        expect(reserveButtonView.el.classList.contains('disable')).toBe(true);
+    });
+
+
+});
